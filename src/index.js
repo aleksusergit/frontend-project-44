59918c467d987288676f9925cefcbb0c492b7e43
aleksandr cherkasin <aleksmail1box@gmail.com>
2022-09-13 19:32:01 +0300

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import readlineSync from 'readline-sync';
 
-const baseOfGame = (nameOfGame, noteToTask, countIter) => {
-  console.log(nameOfGame, '\n');
+const baseOfGame = (nameOfGame, gameCondition, countIter) => {
+  // console.log(nameOfGame, '\n');
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  console.log(noteToTask);
+  console.log(gameCondition);
 
-  for (let iter = 0; iter < 3; iter += 1) {
-    const questionWithResult = countIter();
-    console.log('Question:', questionWithResult[0]);
-    // console.log(questionWithResult[1]);
-    const answerUser = readlineSync.question('Your answer: ');
+  const attemptsCount = 3;
+  for (let iter = 0; iter < attemptsCount; iter += 1) {
+    const [question, correctAnswer] = countIter();
+    console.log(`Question: ${question}`);
+    // console.log(correctAnswer);
+    const userAnswer = readlineSync.question('Your answer: ');
 
-    if (answerUser === questionWithResult[1]) {
-      console.log('Correct!');
-    } else {
-      return console.log(
-        `'${answerUser}' is wrong answer ;(. Correct answer was '${questionWithResult[1]}'.\nLet's try again, ${userName}!`,
-      );
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
+    console.log('Correct!');
   }
-  return console.log(`Congratulations, ${userName}!`);
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default baseOfGame;
